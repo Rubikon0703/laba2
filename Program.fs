b419acf0626699a03a4b9bc60f66c0b61dfc15e8
Rubikon0703 <urasterxov0703@gmail.com>
2026-03-03 08:04:54 +0300

    Update Program.fs

--- Program.fs
+++ Program.fs
@@ -1,41 +1,41 @@
-﻿open System
+open System
 
-//Общая функция ввода списка целых чисел 
-let readIntList () =
+// Общая функция ввода списка целых чисел 
+let ReadIntList () =
     printf "Введите список целых чисел через пробел: "
     let input = Console.ReadLine()
-    input.Split([|' '|], StringSplitOptions.RemoveEmptyEntries)//разбивает строку на подстроки 
-    |> Array.map int   //преобразованует каждую подстроку в int                                      
-    |> List.ofArray     //Преобразует полученный массив чисел в список
+    input.Split([| ' ' |], StringSplitOptions.RemoveEmptyEntries) // Разбивает строку на подстроки 
+    |> Array.map int   // Преобразованует каждую подстроку в int                                      
+    |> List.ofArray     // Преобразует полученный массив чисел в список
 
-//Задание 1 
-let firstDigit (n: int) = //поиск первой цифры числа
+// Задание 1 
+let FirstDigit (n: int) =  // Поиск первой цифры числа
     let absN  = abs n
     let rec loop x =
         if x < 10 then x
         else loop (x / 10)
     loop absN 
 
-let task1 (numbers: int list) =
-    let firstDigits = numbers |> List.map firstDigit 
+let Task1 (numbers: int list) =
+    let firstDigits = numbers |> List.map FirstDigit 
     printfn "Исходный список: %A" numbers
     printfn "Список первых цифр: %A" firstDigits
 
-//Задание 2
-let task2 (numbers: int list) =
+// Задание 2
+let Task2 (numbers: int list) =
     printf "Введите число для подсчёта: "
     let target = Console.ReadLine() |> int
     let count = 
         numbers 
-        |> List.fold (fun acc x -> if x = target then acc + 1 else acc) 0//использует fold для подсчета повторений (сравнивает все числа с введенным)
+        |> List.fold (fun acc x -> if x = target then acc + 1 else acc) 0  // Использует fold для подсчета повторений (сравнивает все числа с введенным)
     printfn "Число %d встречается %d раз(а)" target count
 
-//Точка входа 
+// Точка входа 
 [<EntryPoint>]
 let main argv =
-    let numbers = readIntList ()      
+    let numbers = ReadIntList ()      
     printfn ""
-    task1 numbers                     
+    Task1 numbers                     
     printfn ""
-    task2 numbers                     
+    Task2 numbers                     
     0
